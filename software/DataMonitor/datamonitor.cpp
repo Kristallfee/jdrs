0740e7742e3a4866fdda6cc1ac3e6b081e9d0f5e
Simone Esch <s.esch@fz-juelich.de>
2014-08-08 15:43:06 +0200

    Start of DataMonitor

---
+++ software/DataMonitor/datamonitor.cpp
@@ -0,0 +1,5 @@
+#include "datamonitor.h"
+
+DataMonitor::DataMonitor()
+{
+}
---
+++ software/DataMonitor/datamonitor.h
@@ -0,0 +1,10 @@
+#ifndef DATAMONITOR_H
+#define DATAMONITOR_H
+
+class DataMonitor
+{
+public:
+    DataMonitor();
+};
+
+#endif // DATAMONITOR_H
